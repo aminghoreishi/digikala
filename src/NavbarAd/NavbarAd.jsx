5eec024aa4f80df4bcec0385628603ba2c5348fa
aminghoreishi <162731664+aminghoreishi@users.noreply.github.com>
2025-02-11 20:52:40 +0330

    feat : Navbar

--- src/NavbarAd/NavbarAd.jsx
+++ src/NavbarAd/NavbarAd.jsx
@@ -3,9 +3,9 @@ import "../style.css";
 function NavbarAd() {
   return (
     <div className="bg-yellow-400 p-3">
-      <div className="flex justify-center gap-x-1 items-center font-DanaMedium">
-        <h1>جعبه جادویی هیچ وفت پول نیست!</h1>
-        <p className="font-light text-gray-500 text-sm">تا 235 میلیون تومان جایزه برای همه بدون قرعه کشی</p>
+      <div className="flex justify-center gap-x-1 items-center font-DanaMedium text-xs lg:text-base">
+        <h1 >جعبه جادویی هیچ وفت پول نیست!</h1>
+        <p className="font-light text-gray-500 text-xs hidden lg:flex  lg:text-sm">تا 235 میلیون تومان جایزه برای همه بدون قرعه کشی</p>
       </div>
     </div>
   );
