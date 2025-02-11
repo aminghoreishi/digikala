78d64515585a93ec22ef5c5f763f4b4936a29ac3
aminghoreishi <162731664+aminghoreishi@users.noreply.github.com>
2025-02-11 19:09:29 +0330

    start project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import NavbarAd from "./NavbarAd/NavbarAd";
+import Navbar from "./Navbar/Navbar,";
+function App() {
+  return (
+     <div className="">
+      <NavbarAd/>
+      <Navbar></Navbar>
+     </div>
+  );
+}
+
+export default App;
---
+++ src/NavbarAd/NavbarAd.jsx
@@ -0,0 +1,13 @@
+import "../style.css";
+
+function NavbarAd() {
+  return (
+    <div className="bg-yellow-400 p-3">
+      <div className="flex justify-center gap-x-1 items-center font-DanaMedium">
+        <h1>جعبه جادویی هیچ وفت پول نیست!</h1>
+        <p className="font-light text-gray-500 text-sm">تا 235 میلیون تومان جایزه برای همه بدون قرعه کشی</p>
+      </div>
+    </div>
+  );
+}
+export default NavbarAd;
