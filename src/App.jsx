3b475d7d2a238db1fed6aef862842c22ee212caa
aminghoreishi <162731664+aminghoreishi@users.noreply.github.com>
2025-02-13 13:58:58 +0330

    feat: Main-discount-Category

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import NavbarAd from "./Header/NavbarAd/NavbarAd";
 import Navbar from "./Header/Navbar/Navbar";
 import Header from "./Header/Header";
+import Category from "./Main/Category/Category";
+import Main from "./Main/Main";
 function App() {
   return (
     <div>
       <NavbarAd />
       <Navbar></Navbar>
       <Header/>
+      <Category/>
+      <Main/>
     </div>
   );
 }
---
+++ src/Main/Main.jsx
@@ -0,0 +1,10 @@
+import Category from "./Category/Category";
+import Discount from "./Discount/Discount";
+
+export default function Main() {
+  return (
+    <div className="mt-3">
+      <Discount />
+    </div>
+  );
+}
