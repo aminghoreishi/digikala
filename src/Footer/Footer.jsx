cfbb6692a38c19ab3602d297d1516c47ddf048db
aminghoreishi <162731664+aminghoreishi@users.noreply.github.com>
2025-02-23 22:25:14 +0330

    feat:complete footer

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -2,30 +2,30 @@ import "../style.css";
 import { ArrowLongUpIcon } from "@heroicons/react/24/solid";
 export default function Footer() {
   return (
-    <div className="border-t-2 mt-12 p-3 border-gray-300 font-DanaMedium">
+    <div className="border-t-2 mt-12 max-md:p-2 p-3 border-gray-300 font-DanaMedium">
       <div className="flex items-center justify-between ">
         <div className="">
           <img src="/public/digik.png" alt="" />
         </div>
         <div className="">
-          <button className="flex items-center border-2 p-2 rounded-xl cursor-pointer text-gray-400 text-sm">
+          <button className="flex items-center max-md:text-xs border-2 p-2 rounded-xl cursor-pointer text-gray-400 text-sm">
             <span className="font-light">برگشت به بالا</span>
-            <ArrowLongUpIcon className="size-5" />
+            <ArrowLongUpIcon className="size-5 max-md:size-4" />
           </button>
         </div>
       </div>
-      <div className="text-xs flex divide-x-2 mt-2 divide-gray-400 *:p-1 *:text-gray-500">
+      <div className="text-xs max-md:text-[10px] max-md: max-md:justify-between flex items-center max-md:divide-x-0  max-md:mt-5   divide-x-2 mt-2 divide-gray-400 max-md:*:p-0.5 *:p-1 *:text-gray-500">
         <div className="">
-          <p>تلفن پشتیبانی ۶۱۹۳۰۰۰۰ - ۰۲۱</p>
+          <p className="w-full">تلفن پشتیبانی ۶۱۹۳۰۰۰۰ - ۰۲۱</p>
+        </div>
+        <div className="max-md:hidden">
+          <p className="w-full">۰۲۱-۹۱۰۰۰۱۰۰</p>
         </div>
         <div className="">
-          <p>۰۲۱-۹۱۰۰۰۱۰۰</p>
-        </div>
-        <div className="">
-          <p>۷ روز هفته، ۲۴ ساعته پاسخگوی شما هستیم</p>
+          <p className="w-full">۷ روز هفته، ۲۴ ساعته پاسخگوی شما هستیم</p>
         </div>
       </div>
-      <div className="grid grid-cols-5 mt-5 *:text-gray-500">
+      <div className="grid max-md:grid-cols-2 max-md:gap-3 grid-cols-5 mt-5 *:text-gray-500">
         <div className="text-xs flex flex-col items-center">
           <div className="">
             <img
@@ -72,7 +72,7 @@ export default function Footer() {
           <p>ﺿﻤﺎﻧﺖ اﺻﻞ ﺑﻮدن ﮐﺎﻟﺎ</p>
         </div>
       </div>
-      <div className="grid grid-cols-4 mt-8">
+      <div className="grid max-lg:hidden grid-cols-4 mt-8">
         <div className="">
           <h1>با دیجی کالا</h1>
           <ul className="text-xs text-gray-500 flex flex-col gap-3 mt-2">
@@ -150,26 +150,93 @@ export default function Footer() {
       <div className="mt-8 bg-[#3C4B6D] p-3 rounded-xl text-white">
         <div className="flex justify-between items-center">
           <div className="flex items-center gap-x-3">
-            <div className="size-12">
+            <div className="max-md:size-8 size-12">
               <img
                 className="size-full"
                 src="https://www.digikala.com/statics/img/png/footerlogo2.webp"
                 alt=""
               />
             </div>
-            <p>دانلود اپلیکیشن دیجی کالا</p>
+            <p className="max-md:text-xs max-lg:text-sm">دانلود اپلیکیشن دیجی کالا</p>
           </div>
-          <div className="flex items-center gap-3">
-            <div className="h-[44px]">
-              <img className="size-full" src="https://www.digikala.com/statics/img/svg/appStores/coffe-bazzar.svg" alt="" />
+          <div className="flex items-center max-md:gap-1 gap-3">
+            <div className="h-[44px] max-sm:h-[15px] max-md:h-[20px] max-lg:h-[30px]">
+              <img
+                className="size-full"
+                src="https://www.digikala.com/statics/img/svg/appStores/coffe-bazzar.svg"
+                alt=""
+              />
+            </div>
+            <div className="h-[44px] max-sm:h-[15px] max-md:h-[20px] max-lg:h-[30px]">
+              <img
+                className="size-full"
+                src="https://www.digikala.com/statics/img/svg/appStores/myket.svg"
+                alt=""
+              />
+            </div>
+            <div className="h-[44px] max-sm:h-[15px] max-md:h-[20px] max-lg:h-[30px]">
+              <img
+                className="size-full"
+                src="https://www.digikala.com/statics/img/svg/appStores/sib-app.svg"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="border-t-2 mt-8 border-t-gray-300 p-3">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-3  max-md:gap-5 max-md:justify-center justify-between">
+          <div className="">
+            <p className="max-md:text-base text-xl xl:text-2xl text-gray-500">
+              فروشگاه اینترنتی دیجی کالا ، بررسی ، انتخاب و خرید انلاین
+            </p>
+            <p className="text-xs mt-3 text-gray-400">
+              یک خرید اینترنتی مطمئن، نیازمند فروشگاهی است که بتواند کالاهایی
+              متنوع، باکیفیت و دارای قیمت مناسب را در مدت زمان ی کوتاه به دست
+              مشتریان خود برساند و ضمانت بازگشت کالا هم داشته باشد؛ ویژگی‌هایی
+              که فروشگاه اینترنتی دیجی‌کالا سال‌هاست بر روی آن‌ها کار کرده و
+              توانسته از این طریق مشتریان ثابت خود را داشته باشد.
+            </p>
+          </div>
+          <div className="grid max-md:grid-cols-2 max-md:gap-3 grid-cols-4 items-center *:flex *:justify-center">
+            <div className="">
+              <div className="size-28 p-2 border-2 border-gray-300 rounded-xl">
+                <img
+                  className="size-full"
+                  src="https://www.digikala.com/statics/img/png/sapra.webp"
+                  alt=""
+                />
+              </div>
             </div>
             <div className="">
-              <img src="https://www.digikala.com/statics/img/svg/appStores/myket.svg" alt="" />
+              <div className="size-28 p-2 border-2 border-gray-300 rounded-xl">
+                <img
+                  className="size-full"
+                  src="https://www.digikala.com/statics/img/png/sapra.webp"
+                  alt=""
+                />
+              </div>
             </div>
             <div className="">
-              <img src="https://www.digikala.com/statics/img/svg/appStores/sib-app.svg" alt="" />
+              <div className="size-28 p-2 border-2 border-gray-300 rounded-xl">
+                <img
+                  className="size-full"
+                  src="https://www.digikala.com/statics/img/png/sapra.webp"
+                  alt=""
+                />
+              </div>
             </div>
-            
+            <div className="">
+              <div className="size-28 p-2 border-2 border-gray-300 rounded-xl">
+                <img
+                  className="size-full"
+                  src="https://www.digikala.com/statics/img/png/sapra.webp"
+                  alt=""
+                />
+              </div>
+            </div>
+       
           </div>
         </div>
       </div>
