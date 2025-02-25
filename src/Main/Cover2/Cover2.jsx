f60ec166c006330dc2157881786875f724bf5d75
aminghoreishi <162731664+aminghoreishi@users.noreply.github.com>
2025-02-25 12:25:19 +0330

    refactor: Changes Cover

--- src/Main/Cover2/Cover2.jsx
+++ src/Main/Cover2/Cover2.jsx
@@ -5,8 +5,8 @@ export default function Cover2() {
   return (
     <div className="container mx-auto mt-5">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
-        <CoverItem2 img={"/c1.png"} />
-        <CoverItem2 img={"/moz.png"} />
+        <CoverItem2 img={"https://dkstatics-public.digikala.com/digikala-adservice-banners/0b7ae60f3239ab832c1b57994464d6d71ed3f53f_1739874343.jpg?x-oss-process=image/quality,q_95"} />
+        <CoverItem2 img={"https://dkstatics-public.digikala.com/digikala-adservice-banners/7b4e28de75b6e717eab57c617332b2bd9bfb2c6c_1739794139.jpg?x-oss-process=image/quality,q_95"} />
       </div>
     </div>
   );
