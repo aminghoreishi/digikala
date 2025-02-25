1eca1948658fdc8000916e310531f2d2b08326d8
aminghoreishi <162731664+aminghoreishi@users.noreply.github.com>
2025-02-25 21:24:49 +0330

    fix:deploy

--- src/Main/Brand/BrandSwiper.jsx
+++ src/Main/Brand/BrandSwiper.jsx
@@ -7,31 +7,31 @@ export default function App() {
     <div className="flex justify-around">
       
         <div style={{width:"120px"}}>
-          <img className="w-full" src="%PUBLIC_URL%/peghah.png" alt="" />
+          <img className="w-full" src="https://dkstatics-public.digikala.com/digikala-brands/2c36ceb629598fa31b9b321fcd4e47773bd93ef7_1649498016.png?x-oss-process=image/resize,m_lfit,h_160,w_160/quality,q_80" alt="" />
         </div>
         <div style={{width:"120px"}}>
-          <img className="w-full" src="%PUBLIC_URL%/huawi.png" alt="" />
+          <img className="w-full" src="https://dkstatics-public.digikala.com/digikala-brands/7d615272ba7fc5dd708195ae858e2971de86a06f_1649498033.png?x-oss-process=image/resize,m_lfit,h_160,w_160/quality,q_80" alt="" />
         </div>
         <div style={{width:"120px"}}>
-          <img className="w-full" src="%PUBLIC_URL%/xvi.png" alt="" />
+          <img className="w-full" src="https://dkstatics-public.digikala.com/digikala-brands/9f5727fd7c13dc4b10c879a1865d77bc1fb333ce_1705308969.jpg?x-oss-process=image/resize,m_lfit,h_160,w_160/quality,q_80" alt="" />
         </div>
         <div style={{width:"120px"}}>
-          <img className="w-full" src="%PUBLIC_URL%/anter.png" alt="" />
+          <img className="w-full" src="https://dkstatics-public.digikala.com/digikala-brands/87dcb454f4d8df28bd2f8fd902f0f27b5fb1fac4_1666761801.jpg?x-oss-process=image/resize,m_lfit,h_160,w_160/quality,q_80" alt="" />
         </div>
         <div style={{width:"120px"}}>
-          <img className="w-full" src="%PUBLIC_URL%/china.png" alt="" />
+          <img className="w-full" src="https://dkstatics-public.digikala.com/digikala-brands/5a10ae95eb42fc96906c8062563b28062668d418_1722238640.png?x-oss-process=image/resize,m_lfit,h_160,w_160/quality,q_80" alt="" />
         </div>
         <div style={{width:"120px"}}>
-          <img className="w-full" src="%PUBLIC_URL%/kingstar.png" alt="" />
+          <img className="w-full" src="https://dkstatics-public.digikala.com/digikala-brands/3f0e8a262651232007912c06d7cd69f7d9b4bfa4_1662306119.jpg?x-oss-process=image/resize,m_lfit,h_160,w_160/quality,q_80" alt="" />
         </div>
         <div style={{width:"120px"}}>
-          <img className="w-full" src="%PUBLIC_URL%/per.png" alt="" />
+          <img className="w-full" src="https://dkstatics-public.digikala.com/digikala-brands/960dc4b74e21a9d33d594a161386179b26b3a68a_1606823783.png?x-oss-process=image/resize,m_lfit,h_160,w_160/quality,q_80" alt="" />
         </div>
         <div style={{width:"120px"}}>
-          <img className="w-full" src="%PUBLIC_URL%/china.png" alt="" />
+          <img className="w-full" src="https://dkstatics-public.digikala.com/digikala-brands/e44886020b773202d6d431e2fef2f3bd8bd3bc7f_1737817444.jpg?x-oss-process=image/resize,m_lfit,h_160,w_160/quality,q_80" alt="" />
         </div>
         <div style={{width:"120px"}}>
-          <img className="w-full" src="%PUBLIC_URL%/china.png" alt="" />
+          <img className="w-full" src="https://dkstatics-public.digikala.com/digikala-brands/d23e8875d5f7a9735e433f1fd86a1513902efc8b_1604470597.jpg?x-oss-process=image/resize,m_lfit,h_160,w_160/quality,q_80" alt="" />
         </div>
       
     </div>
